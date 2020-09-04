7b10e1381607ebdfd3b56731dea936ab5e1fbd8e
Farouk Jabberi <jabberi.contact@gmail.com>
2020-09-04 13:40:35 +0200

    Add the retrofit api

---
+++ app/src/main/java/com/training/mvvm/data/PostInterface.kt
@@ -0,0 +1,10 @@
+package com.training.mvvm.data
+
+import com.training.mvvm.pojo.PostModel
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface PostInterface {
+    @GET("posts")
+    fun getPost(): Call<PostModel>
+}
