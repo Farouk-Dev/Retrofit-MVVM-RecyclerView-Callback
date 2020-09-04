052efe9fc364b75cc1ee45030625f46f3ac9b18c
Farouk Jabberi <jabberi.contact@gmail.com>
2020-09-04 17:38:03 +0200

    Fix crash

--- app/src/main/java/com/training/mvvm/data/PostClient.kt
+++ app/src/main/java/com/training/mvvm/data/PostClient.kt
@@ -1,18 +1,22 @@
 package com.training.mvvm.data
 
+import android.util.Log
 import com.training.mvvm.pojo.PostModel
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
+private lateinit var postInterface: PostInterface
+
 class PostClient {
-    private lateinit var postInterface: PostInterface
 
     companion object {
         const val BASE_URL = "https://jsonplaceholder.typicode.com/"
-        private lateinit var INSTANCE: PostClient
-        fun getInstance(): PostClient {
-            if (INSTANCE == null) INSTANCE = PostClient()
+        private var INSTANCE: PostClient? = null
+        fun getInstance(): PostClient? {
+            if (INSTANCE == null)
+                INSTANCE = PostClient()
             return INSTANCE
         }
     }
@@ -24,7 +28,11 @@ class PostClient {
         postInterface = retrofit.create(PostInterface::class.java)
     }
 
-    fun getPosts(): Call<PostModel> {
+    fun getPosts(): Call<List<PostModel>> {
         return postInterface.getPost()
     }
+
 }
+
+
+
